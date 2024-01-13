5abbe7d1419e63a3ba0a5b0419da5813b3999c14
iypavlov <iy.pavlov@yandex.ru>
2024-01-13 20:13:33 +0300

    bot class

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('init');
+import { ConfigService } from './config/config.service';
+import { IConfigService } from './config/config.interface';
+import { session, Telegraf } from 'telegraf';
+
+class Bot {
+  bot: Telegraf;
+
+  constructor(private readonly configService: IConfigService) {
+    this.bot = new Telegraf(this.configService.get('TOKEN'));
+    this.bot.use(session());
+  }
+
+  init() {
+    this.bot.launch();
+  }
+}
+
+const bot = new Bot(new ConfigService());
+bot.init();
